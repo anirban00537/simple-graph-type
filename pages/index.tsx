837246daca610429ea84bca898664592e8357751
anirban00537 <anirban00537@gmail.com>
2022-01-30 00:14:12 +0600

    fixed problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
-import type { NextPage, GetStaticProps } from "next";
-import { gql } from "@apollo/client";
+import type { GetServerSideProps } from "next";
 import { apolloClient } from "../config/graph_setup";
 import styles from "../styles/Home.module.css";
 import { CHARECTER } from "../graphql/query/charecers";
 
-const Home: NextPage = () => {
+type Props = {
+  characters: Array<any>;
+};
+
+const Home = ({ characters }: Props) => {
+  console.log(characters, "characters");
   return <div className={styles.container}></div>;
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const { data } = await apolloClient.query({
     query: CHARECTER,
   });
-  console.log(data, "this is a data");
+  console.log(data.characters, "sas");
   return {
     props: {
-      charecters: data,
+      characters: data.characters.results,
     },
   };
 };
